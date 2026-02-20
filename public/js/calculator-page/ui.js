6923e1d18b9f384b50b8d5f88fe83bdf9f3d555f
Vadym Popoilyk <vadym@vadyms-mbp.home>
2026-02-21 00:12:38 +0100

    responsiveness fix

--- public/js/calculator-page/ui.js
+++ public/js/calculator-page/ui.js
@@ -2,7 +2,6 @@ import { state } from './state.js';
 
 export const screen = document.querySelector('.result');
 
-
 export function updateScreen() {
     screen.textContent = state.expression || '0';
     screen.parentElement.scrollLeft = screen.parentElement.scrollWidth;
