b698d9d3feb80924770950d07f0c35959ed0bca1
Vadym Popoilyk <vadym@vadyms-mbp.home>
2026-02-23 18:01:45 +0100

    responsivennes fix 2.1

--- public/js/zoom.js
+++
@@ -1,9 +0,0 @@
-let lastTouchEnd = 0;
-
-document.addEventListener('touchend', function (event) {
-  const now = Date.now();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
-}, { passive: false });
